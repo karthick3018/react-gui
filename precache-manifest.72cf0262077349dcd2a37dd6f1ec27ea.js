323391bdeb19654de91f5c313961d5e43fc82604
karthick3018 <karthickraja3018@gmail.com>
2020-11-17 12:06:26 +0530

    Updates

---
+++ precache-manifest.72cf0262077349dcd2a37dd6f1ec27ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd2ab2a597a9112033413d15259142f",
+    "url": "/react-gui/index.html"
+  },
+  {
+    "revision": "38bd1c4401fd5934013d",
+    "url": "/react-gui/static/css/3.8d197ef7.chunk.css"
+  },
+  {
+    "revision": "dfb9e63fca25da87b6ec",
+    "url": "/react-gui/static/css/main.808f7f97.chunk.css"
+  },
+  {
+    "revision": "f4c603b2e9e6ba4197e1",
+    "url": "/react-gui/static/js/2.d073bb80.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gui/static/js/2.d073bb80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38bd1c4401fd5934013d",
+    "url": "/react-gui/static/js/3.08bd505e.chunk.js"
+  },
+  {
+    "revision": "dfb9e63fca25da87b6ec",
+    "url": "/react-gui/static/js/main.43a7afd3.chunk.js"
+  },
+  {
+    "revision": "834ace98ee5c995748e8",
+    "url": "/react-gui/static/js/runtime-main.adc994c6.js"
+  },
+  {
+    "revision": "6ac51f049b4c3aad36706cde19956357",
+    "url": "/react-gui/static/media/menu.6ac51f04.svg"
+  }
+]);
